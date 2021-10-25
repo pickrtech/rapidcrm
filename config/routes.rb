2dcbd0e204a96e1bbcbafc51a5f30c7db08a4953
DebasishMohanta <debasish2018rta@gmail.com>
2021-10-25 14:19:49 +0530

    Adding welcome controller and its Views Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root :to => "welcome#home"
+  get 'welcome/home'
+  get 'welcome/about'
+  get 'welcome/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
